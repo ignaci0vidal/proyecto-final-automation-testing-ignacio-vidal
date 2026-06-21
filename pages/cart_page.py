009ff0f6e3a270ddd7398e5c73243f21d60f3122
ignacio vidal <ignaciovidal@gmail.com>
2026-06-21 19:51:29 -0300

    Robustece eliminación de productos en modo headless

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -1,11 +1,20 @@
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
 class CartPage:
-    CART_ITEMS = (By.CLASS_NAME, "cart_item")
-    ITEM_NAMES = (By.CLASS_NAME, "inventory_item_name")
+    CART_ITEMS = (
+        By.CLASS_NAME,
+        "cart_item"
+    )
+
+    ITEM_NAMES = (
+        By.CLASS_NAME,
+        "inventory_item_name"
+    )
+
     REMOVE_BACKPACK_BUTTON = (
         By.ID,
         "remove-sauce-labs-backpack"
@@ -13,7 +22,7 @@ class CartPage:
 
     def __init__(self, driver):
         self.driver = driver
-        self.wait = WebDriverWait(driver, 10)
+        self.wait = WebDriverWait(driver, 15)
 
     def obtener_nombres_productos(self):
         elementos = self.wait.until(
@@ -22,12 +31,16 @@ class CartPage:
             )
         )
 
-        return [elemento.text for elemento in elementos]
+        return [
+            elemento.text
+            for elemento in elementos
+        ]
 
     def obtener_cantidad_productos(self):
         productos = self.driver.find_elements(
             *self.CART_ITEMS
         )
+
         return len(productos)
 
     def eliminar_backpack(self):
@@ -44,8 +57,31 @@ class CartPage:
 
         boton.click()
 
-        self.wait.until(
-            EC.invisibility_of_element_located(
-                self.REMOVE_BACKPACK_BUTTON
+        try:
+            self.wait.until(
+                lambda driver: len(
+                    driver.find_elements(
+                        *self.CART_ITEMS
+                    )
+                ) == 0
             )
-        )
+
+        except TimeoutException:
+            boton = self.wait.until(
+                EC.presence_of_element_located(
+                    self.REMOVE_BACKPACK_BUTTON
+                )
+            )
+
+            self.driver.execute_script(
+                "arguments[0].click();",
+                boton
+            )
+
+            self.wait.until(
+                lambda driver: len(
+                    driver.find_elements(
+                        *self.CART_ITEMS
+                    )
+                ) == 0
+            )
